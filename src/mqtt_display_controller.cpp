6ecbf8d18b1cc9b4be018a9b47cb2874538b43b1
Brenden Crowie <brenden@crowie.dev>
2021-07-05 00:43:50 -0400

    Switched to clickencoder library, fixed sensitivity

--- src/mqtt_display_controller.cpp
+++ src/mqtt_display_controller.cpp
@@ -1,34 +1,38 @@
 #include <Wire.h>
 #include <ESP8266WiFi.h>
-#include <RotaryEncoder.h>
+#include <ClickEncoder.h>
 #include "SSD1306Ascii.h"
 #include "SSD1306AsciiWire.h"
 #include <string>
 
-// OLED Display Definitions
+// OLED
 #define I2C_ADDRESS 0x3C
 #define RST_PIN -1
 
-// WiFi Definitions
-#define WIFI_SSID ""
-#define WIFI_PASSWORD ""
+// WiFi
+#define WIFI_SSID "BHN-Lashley"
+#define WIFI_PASSWORD "W@terL00"
 SSD1306AsciiWire oled;
 
-// Rotary Encoder Definitions
-#define PIN_A D5
-#define PIN_B D4
-#define BUTTON D3
-int16_t position = 0;
-RotaryEncoder encoder(PIN_A, PIN_B, BUTTON);
-
+// Rotary Encoder
+#define ENCODER_PINA     D5
+#define ENCODER_PINB     D4
+#define ENCODER_BTN      D3
+#define ENCODER_STEPS_PER_NOTCH     2
+ClickEncoder encoder = ClickEncoder(ENCODER_PINA,ENCODER_PINB,ENCODER_BTN,ENCODER_STEPS_PER_NOTCH);
 
 //------------------------------------------------------------------------------
 
-String menu[] = {
+int8 position = 0;
+
+String menu[7] = {
   "Menu item one",
   "Menu item 2",
   "Menu item 333",
-  "Menu item 44444"
+  "Menu item 44444",
+  "5",
+  "6",
+  "Brenden"
 };
 
 void updateDisplay(String data)
@@ -62,47 +66,31 @@ bool connectWifi(String SSID, String PASSWORD)
   return true;
 }
 
-void ICACHE_RAM_ATTR encoderISR()
-{
-  encoder.readAB();
-}
-
-void ICACHE_RAM_ATTR encoderButtonISR()
-{
-  encoder.readPushButton();
-}
-
-void updateMenu() {
-  // Update menu code
-}
-
 void setup() {
-  // Start serial for monitoring
   Serial.begin(115200);
-  Serial.println();
   
   Wire.begin();
   Wire.setClock(400000L);
 
-  encoder.begin();                                                           //set encoders pins as input & enable built-in pullup resistors
+  encoder.setButtonHeldEnabled(true);
+  encoder.setDoubleClickEnabled(true);
+  encoder.setButtonOnPinZeroEnabled(true);
 
-  attachInterrupt(digitalPinToInterrupt(PIN_A),  encoderISR,       CHANGE);  //call encoderISR()       every high->low or low->high changes
-  attachInterrupt(digitalPinToInterrupt(BUTTON), encoderButtonISR, FALLING);
 
-#if RST_PIN >= 0
-  oled.begin(&Adafruit128x64, I2C_ADDRESS, RST_PIN);
-#else 
-  oled.begin(&Adafruit128x64, I2C_ADDRESS);
-#endif
+  if(RST_PIN >= 0){
+    oled.begin(&Adafruit128x64, I2C_ADDRESS, RST_PIN);
+  } else {
+    oled.begin(&Adafruit128x64, I2C_ADDRESS);
+  }
 
   oled.setFont(Adafruit5x7);
 
-  if(connectWifi(WIFI_SSID, WIFI_PASSWORD))
-  {
+  // Pub/Sub MQTT
+
+  if(connectWifi(WIFI_SSID, WIFI_PASSWORD)) {
     oled.clear();
     oled.set2X();
-
-    oled.println("Ready!!"); // Change to update display with menu
+    oled.println("Ready!!"); // Change to update display with menu after wifi connect and mqtt subscription
     delay(2000);
     oled.set1X();
     oled.clear();
@@ -110,28 +98,41 @@ void setup() {
 }
 //------------------------------------------------------------------------------
 void loop() {
-  // Check for rotarty rotation, button presses then update display
-  if (position != encoder.getPosition())
-  {
-    position = encoder.getPosition();
+  static uint32_t lastService = 0;
+  if (lastService + 1000 < micros()) {
+    lastService = micros();                
+    encoder.service();  
+  }
 
-    if(position > sizeof(menu) - 1 ) 
-    {
-      position = 0;
+  static int16_t last, value;
+    value += encoder.getValue();
+  
+  #define NUMITEMS(arg) ((unsigned int) (sizeof (arg) / sizeof (arg [0]))) - 1
+
+  if (value != last) {
+    last = value;
+
+    if(value > NUMITEMS(menu)){
+      value = 0;
     } 
-    else if (position <= -1) 
-    {
-      position = sizeof(menu) - 1;
-    };
-    
-    updateDisplay(menu[position]);
+    if (value < 0) {
+      value = NUMITEMS(menu);
+      last = value;
+    }
+
+    updateDisplay(menu[value]);
   }
 
-  if (encoder.getPushButton() == true)
-  {
-    updateDisplay("PRESSED");
-    //oled.println("PRESSED");
-    //delay(2000);
-    //oled.clear();
-  }
+    ClickEncoder::Button b = encoder.getButton();
+  if (b != ClickEncoder::Open) {
+    Serial.print("Button: ");
+    #define VERBOSECASE(label) case label: updateDisplay(#label); break;
+    switch (b) {
+      VERBOSECASE(ClickEncoder::Pressed);
+      VERBOSECASE(ClickEncoder::Held)
+      VERBOSECASE(ClickEncoder::Released)
+      VERBOSECASE(ClickEncoder::Clicked)
+      VERBOSECASE(ClickEncoder::DoubleClicked)
+    }
+  }    
 }
